d7319350978a100948c19f5fe564942250167391
Theo Paris <theoplaysmc@gmail.com>
2021-02-02 17:36:53 -0800

    using gson instead of org.json

--- src/main/kotlin/dev/throwouterror/util/data/JsonBuilder.kt
+++ src/main/kotlin/dev/throwouterror/util/data/JsonBuilder.kt
@@ -1,22 +1,24 @@
 package dev.throwouterror.util.data
 
-import org.json.JSONObject
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import java.util.*
+import com.google.gson.*
 
-fun json(build: JsonObjectBuilder.() -> Unit): JSONObject {
-    return JsonObjectBuilder().json(build)
+fun json(build: JsonObjectBuilder.() -> Unit): JsonObject {
+    return JsonObjectBuilder().json(build).asJsonObject
 }
 
-class JsonObjectBuilder {
-    private val deque: Deque<JSONObject> = ArrayDeque()
+class JsonObjectBuilder(val gson: Gson = GsonBuilder().setPrettyPrinting().create()) {
+    private val deque: Deque<JsonElement> = ArrayDeque()
 
-    fun json(build: JsonObjectBuilder.() -> Unit): JSONObject {
-        deque.push(JSONObject())
+    fun json(build: JsonObjectBuilder.() -> Unit): JsonElement {
+        deque.push(JsonObject())
         this.build()
         return deque.pop()
     }
 
     infix fun <T> String.to(value: T) {
-        deque.peek().put(this, value)
+        deque.peek().asJsonObject.add(this, gson.toJsonTree(value))
     }
 }
